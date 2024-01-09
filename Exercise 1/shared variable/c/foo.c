b46861f3d2500a765b729ebd2cfa4f86636f7697
name <email@example.com>
2024-01-09 15:27:06 +0100

    oppgave 3

--- Exercise 1/shared variable/c/foo.c
+++ Exercise 1/shared variable/c/foo.c
@@ -9,24 +9,39 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    for (int j=0; j<1000000; j++) {
+        i++;
+    }
+
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    for (int j=0; j<1000000; j++) {
+        i--;
+    }
     return NULL;
 }
+
 
 
 int main(){
     // TODO: 
     // start the two functions as their own threads using `pthread_create`
     // Hint: search the web! Maybe try "pthread_create example"?
-    
+
+    pthread_t thread1, thread2; //initializing threads
+
+    pthread_create(&thread1, NULL, incrementingThreadFunction, NULL); //standard attributes and func with no args
+    pthread_create(&thread2, NULL, decrementingThreadFunction, NULL); 
     // TODO:
     // wait for the two threads to be done before printing the final result
     // Hint: Use `pthread_join`    
-    
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
     printf("The magic number is: %d\n", i);
     return 0;
 }
